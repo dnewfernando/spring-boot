32c0cd15426e4ffa8cbde62a23954e18b51f80d8
dnewfernando <dnewfernando@gmail.com>
2026-06-16 14:17:02 +0700

    Create PrApplication.java

---
+++ pr/src/main/java/course/danu/pr/PrApplication.java
@@ -0,0 +1,13 @@
+package course.danu.pr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrApplication.class, args);
+	}
+
+}
